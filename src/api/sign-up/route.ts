5b4037a846999842f0e874a8ff3c1542e821fb5b
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2024-06-10 09:03:37 +0530

    commit

--- src/api/sign-up/route.ts
+++ src/api/sign-up/route.ts
@@ -31,7 +31,20 @@ export async function POST(request: Request) {
             ({ email })
         const verifyCode = Math.floor(100000 + Math.random()*900000).toString()
         if (existingUserByEmail) {
-            true
+            if(existingUserByEmail.isVerified){
+                return Response.json({
+                    success: false,
+                    message: "User already exist with this email"
+                }, {
+                    status: 400
+                })
+            } else {
+                const hasedPassword = await bcrypt.hash(password, 10)
+                existingUserByEmail.password =  hasedPassword;
+                existingUserByEmail.verifyCode = verifyCode;
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+                await existingUserByEmail.save()
+            }
         } else {
             const hasedPassword = await bcrypt.hash(password, 10)
             const expiryDate = new Date()
@@ -49,6 +62,27 @@ export async function POST(request: Request) {
             })
             await newUser.save()
         }
+        // send verification email
+        const emailResponse = await sendVerificationEmail(
+            email,
+            username,
+            verifyCode,
+        )
+        if(!emailResponse.success) { 
+            return Response.json({
+                success: false,
+                message: emailResponse.message
+            }, {
+                status: 500
+            })
+        }
+
+        return Response.json({
+            success: true,
+            message: "User registered successfully, Please verify your email"
+        }, {
+            status: 201
+        })
     } catch (error) {
         console.error('Error registering user', error)
         return Response.json({
